b8b12e6fbaeade44939dce5292dbd4deff9f2ac8
xingt-tang <tangxing01@gmail.com>
2025-10-11 14:00:14 +0000

    Deploying to gh-pages from @ xingt-tang/xingt-tang.github.io@24f7622b7f86fea8b821df82f3b956e24ccc3e4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Fundamentals of Programming Language",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
